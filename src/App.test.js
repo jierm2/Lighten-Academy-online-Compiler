e218b33888387fc097183731b8b756572c604d0b
Jimmy Miao <jimmymiao08@gmail.com>
2023-07-05 17:26:55 -0400

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders home title', () => {
+  // Render the Home component
+  render(<Home />);
+
+  // Check if the home title is rendered
+  const homeTitle = screen.getByText('Python Playground');
+  expect(homeTitle).toBeInTheDocument();
+});
