9d41a05a02e3f6db662c059f5b22c16b3ba898ae
Jimmy Miao <jimmymiao08@gmail.com>
2023-07-05 17:29:56 -0400

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Home from './Home';
+import Home from './pages/Home';
 
 test('renders home title', () => {
   // Render the Home component
