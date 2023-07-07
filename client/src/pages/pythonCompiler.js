13633b0ac8b2432a0abc50acd972b9dc6425eabd
Jimmy Miao <jimmymiao08@gmail.com>
2023-07-06 21:51:58 -0400

    .

--- client/src/pages/pythonCompiler.js
+++ client/src/pages/pythonCompiler.js
@@ -15,7 +15,7 @@ function PythonCompiler() {
   
   const handleRunCode = useCallback(async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/execute', {
+      const response = await fetch('http:// 172.31.25.5:5000/execute', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- client/src/pages/pythonTest.js
+++ client/src/pages/pythonTest.js
@@ -22,7 +22,7 @@ function PythonTest({ initialCode, setResult, id }) {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/execute', {
+      const response = await fetch('http://172.31.25.5:5000/execute', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
