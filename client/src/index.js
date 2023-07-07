765eb5a06d6ea4fae292f1ec92c7bfe8bd839818
Jimmy Miao <jimmymiao08@gmail.com>
2023-07-06 22:10:05 -0400

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';  // Import the AuthProvider component
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <AuthProvider>  {/* Wrap the App component with AuthProvider */}
       <App />
     </AuthProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
